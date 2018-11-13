7e5450b6bebc307bf4637d852a81d727c7d6f872
hnyize <hainanyize@qq.com>
2018-11-13 18:56:47 +0800

    modifed

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ echo "staring nginx for webUI..."
 nginx
 if [ "${secret}"="" ] then
     echo "staring aria2 without password..."
-    aria2c --enable-rpc --rp-clisten-all
+    aria2c --enable-rpc --rp-clisten-all --dir=/download
 else
     echo "staring aria2 with password..."
-    aria2c --rpc-secret=${secret} --enable-rpc --rp-clisten-all
+    aria2c --rpc-secret=${secret} --enable-rpc --rp-clisten-all --dir=/download
 fi
