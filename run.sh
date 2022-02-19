7ca253de7dae3b9006ec5a1649201d46d8ab26c5
hnyize <44634443+hnyize@users.noreply.github.com>
2022-02-19 18:54:06 +0800

    add dns server

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "staring nginx for webUI..."
 /usr/sbin/nginx
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 if [ "${secret}" = "" ]; then
     echo "staring aria2 without password..."
     aria2c --enable-rpc --rpc-listen-all --dir=/download
